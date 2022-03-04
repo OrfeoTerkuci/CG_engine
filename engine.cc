2f0bf344057f274c706fb51aff6d9ca048aea9bb
OrfeoTerkuci <terkuciorfeo@gmail.com>
2022-03-04 17:27:10 +0100

    LSystems

--- engine.cc
+++ engine.cc
@@ -1,6 +1,8 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "bugprone-narrowing-conversions"
 #include "easy_image.h"
 #include "ini_configuration.h"
-
+#include "l_parser/l_parser.h"
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
@@ -16,12 +18,16 @@ public:
     double red;
     double green;
     double blue;
+
+    Color(double red, double green, double blue) : red(red), green(green), blue(blue) {}
 };
 
 class Point2D {
 public:
     double x;
     double y;
+
+    Point2D(double x, double y) : x(x), y(y) {}
 };
 
 class Line2D {
@@ -29,9 +35,11 @@ public:
     Point2D p1;
     Point2D p2;
     Color color;
+
+    Line2D(const Point2D &p1, const Point2D &p2, const Color &color) : p1(p1), p2(p2), color(color) {}
 };
 
-using Lines2D = list<Line2D>;
+using Lines2D = vector<Line2D>;
 // Main functionality
 
 vector<unsigned int> scaleColors(vector<double> &originalColor){
@@ -198,45 +206,151 @@ img::EasyImage createDiamond(int &imageWidth, int &imageHeight, int &linesNumber
     return image;
 }
 
-img::EasyImage draw2DLines (const Lines2D &lines , const int size){
+img::EasyImage draw2DLines (const Lines2D &lines , const int size , vector<double> &backgroundColor){
+    // Declare colors vector
+    vector<double> originalColors;
+    vector<unsigned int> newColors;
+    vector<unsigned int> bgColor = scaleColors(backgroundColor);
     // Determine x_min , y_min , x_max , y_max
     double x_min = 0, y_min = 0, x_max = 0, y_max = 0;
-    double x1 , y1 , x2 , y2;
+    //double p1_x ; p1_y , p2_x , p2_y;
     // Loop through all the lines
-    for( Line2D l : lines){
+    for( Line2D l : lines) {
         // Assign the points to temp variables
-        x1 = l.p1.x;
-        y1 = l.p1.y;
-        x2 = l.p2.x;
-        y2 = l.p2.y;
+        double p1_x = l.p1.x;
+        double p1_y = l.p1.y;
+        double p2_x = l.p2.x;
+        double p2_y = l.p2.y;
         // Check coordinates of the first point
-        if (x_max <= x1){
-            x_max = x1;
+        if (x_max <= p1_x){
+            x_max = p1_x;
         }
-        if (x_min >= x1){
-            x_min = x1;
+        if (x_min >= p1_x){
+            x_min = p1_x;
         }
-        if (y_max <= y1){
-            y_max = y1;
+        if (y_max <= p1_y){
+            y_max = p1_y;
         }
-        if (y_min >= y1){
-            y_min = y1;
+        if (y_min >= p1_y){
+            y_min = p1_y;
         }
         // Check coordinates of the second point
-        if (x_max <= x2){
-            x_max = x2;
+        if (x_max <= p2_x){
+            x_max = p2_x;
         }
-        if (x_min >= x2){
-            x_min = x2;
+        if (x_min >= p2_x){
+            x_min = p2_x;
         }
-        if (y_max <= y2){
-            y_max = y2;
+        if (y_max <= p2_y){
+            y_max = p2_y;
         }
-        if (y_min >= y2){
-            y_min = y2;
+        if (y_min >= p2_y){
+            y_min = p2_y;
         }
     }
 
+    // Calculate range along the x-axis and y-axis
+    double x_range = x_max - x_min;
+    double y_range = y_max - y_min;
+    // Calculate image dimentions
+    double imageWidth   = size * (x_range / max(x_range,y_range));
+    double imageHeight  = size * (y_range / max(x_range,y_range));
+    // Create the image file
+    img::EasyImage image(lround(imageWidth) , lround(imageHeight));
+    image.clear(img::Color(bgColor.at(0) , bgColor.at(1) , bgColor.at(2) ) );
+    // Determine the scaling factor
+    double d = 0.95 * (imageWidth / x_range);
+    // Calculate for x and y
+    double DC_x = d * ( (x_min + x_max) / 2 );
+    double DC_y = d * ( (y_min + y_max) / 2 );
+    double dx = (imageWidth / 2) - DC_x;
+    double dy = (imageHeight / 2) - DC_y;
+    // Declare temporary variables
+    //int x1 , x2 , y1 , y2;
+    // Loop through the lines again
+    for (Line2D l : lines) {
+        // Multiply all the points by the scaling factor
+        l.p1.x  *= d;
+        l.p1.y  *= d;
+        l.p2.x  *= d;
+        l.p2.y  *= d;
+        // Add dx and dy to each point's coordinate
+        l.p1.x += dx;
+        l.p1.y += dy;
+        l.p2.x += dx;
+        l.p2.y += dy;
+        // Round the coordinates
+        int x1 = lround(l.p1.x);
+        int x2 = lround(l.p2.x);
+        int y1 = lround(l.p1.y);
+        int y2 = lround(l.p2.y);
+        // Fetch and rescale the colors
+        originalColors = {l.color.red , l.color.green , l.color.blue};
+        newColors = scaleColors(originalColors);
+        // Draw the lines
+        image.draw_line(x1 , y1 , x2 , y2 , img::Color(newColors.at(0) , newColors.at(1) , newColors.at(2) ) );
+    }
+    return image;
+}
+
+Lines2D drawSystem (const LParser::LSystem2D &l_system , const int &size , vector<double> &backgroundColor , vector<double> &lineColor) {
+    // Create the list of lines
+    Lines2D lines;
+
+    // Get all the components of the LSystem
+    auto Alphabet = l_system.get_alphabet();
+    string initiator = l_system.get_initiator();
+    double angle = l_system.get_angle();
+    double startingAngle = l_system.get_starting_angle();
+
+    // Convert angles to radians
+    angle = ( angle * M_PI ) / 180;
+    startingAngle = ( startingAngle * M_PI ) / 180;
+
+    // Get inierations
+    unsigned int initerations = l_system.get_nr_iterations();
+
+    // Get initiating string
+    string startingString = l_system.get_replacement(initiator[0]);
+    string endingString;
+    // Replace symbols
+    for( int i = 0; i < initerations; i++){
+        for(char c : startingString){
+            if(c != '-' && c != '+' && c != '(' && c != ')'){
+                endingString += l_system.get_replacement(c);
+            }
+        }
+    }
+    // Make a reference point
+    Point2D currentPoint(0,0);
+
+    // Loop through characters in initiating string
+    for( char c : endingString) {
+        // If angle must increase
+        if (c == '+') {
+            startingAngle += angle;
+        }
+        // If angle must decrease
+        else if (c == '-') {
+            startingAngle -= angle;
+        }
+        // If we must draw
+        else if(l_system.draw(c)){
+            Line2D line(    Point2D(currentPoint.y , currentPoint.y) ,
+                            Point2D(currentPoint.x +  cos(startingAngle) , currentPoint.y + sin(startingAngle)) ,
+                            Color(lineColor[0] , lineColor[1] , lineColor[2])
+                            );
+            lines.push_back(line);
+            currentPoint.x += cos(startingAngle);
+            currentPoint.y += sin(startingAngle);
+        }
+        // If we mustn't draw
+        else {
+            currentPoint.x += cos(startingAngle);
+            currentPoint.y += sin(startingAngle);
+        }
+    }
+    return lines;
 }
 
 img::EasyImage generate_image(const ini::Configuration &configuration)
@@ -248,7 +362,19 @@ img::EasyImage generate_image(const ini::Configuration &configuration)
 
     // Get type
     string type = configuration["General"]["type"].as_string_or_die();
-
+    // Case : type == "2DLSystem"
+    if (type == "2DLSystem"){
+        int size = configuration["General"]["size"].as_int_or_die();
+        vector<double> backgroundColor = configuration["General"]["backgroundcolor"];
+        string input_filename = configuration["2DLSystem"]["inputfile"];
+        vector<double> lineColor = configuration["2DLSystem"]["color"];
+        // Initialize the parser
+        LParser::LSystem2D l_system;
+        ifstream input_stream(input_filename);
+        input_stream >> l_system;
+        input_stream.close();
+        return draw2DLines( drawSystem(l_system , size , backgroundColor , lineColor ) , size , backgroundColor );
+    }
     /*
      * [ImageProperties]
      * width = width_of_image
@@ -288,14 +414,6 @@ img::EasyImage generate_image(const ini::Configuration &configuration)
         if(figure == "Diamond"){
             return createDiamond(width,height,linesNumber,backgroundColor,lineColor);
         }
-
-    }
-    // Case : type == "2DLSystem"
-    if (type == "2DLSystem"){
-        int size = configuration["General"]["size"].as_int_or_die();
-        vector<double> backgroundColor = configuration["General"]["backgroundcolor"];
-        string input_filename = configuration["2DLSystem"]["inputfile"];
-        vector<double> lineColor = configuration["2DLSystem"]["color"];
 
     }
 
@@ -376,3 +494,5 @@ int main(int argc, char const* argv[])
         }
         return retVal;
 }
+
+#pragma clang diagnostic pop
